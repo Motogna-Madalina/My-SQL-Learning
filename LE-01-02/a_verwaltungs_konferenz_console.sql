42b6b68dad5bca684983eaaac6a2bd246b4b08ad
Madalina Motogna <054093@edu.szf.at>
2026-04-27 10:03:41 +0200

    update

--- LE-01-02/a_verwaltungs_konferenz_console.sql
+++ LE-01-02/a_verwaltungs_konferenz_console.sql
@@ -1,31 +1,30 @@
-CREATE DATABASE verwaltung_konferenz;
+DROP DATABASE IF EXISTS verwaltung_konferenz;
+CREATE DATABASE IF NOT EXISTS verwaltung_konferenz;
 
 USE verwaltung_konferenz;
 
-CREATE TABLE referent (
-id_referent INT NOT NULL PRIMARY KEY ,
+CREATE TABLE IF NOT EXISTS referent (
+id_referent INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 name VARCHAR(100) NOT NULL,
-e_mail VARCHAR(255) NOT NULL,
+e_mail VARCHAR(255) NOT NULL UNIQUE ,
 organisation VARCHAR(100) DEFAULT NULL
 );
 
-CREATE TABLE teilnehmer (
+CREATE TABLE IF NOT EXISTS teilnehmer (
   id_teilnehmer INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   e_mail VARCHAR(255) NOT NULL UNIQUE ,
   organisation VARCHAR(100) DEFAULT NULL
 );
 
-CREATE TABLE themen (
-  id_themen INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+CREATE TABLE IF NOT EXISTS themen (
+  id_themen INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   kurzbeschreibung VARCHAR(300) NOT NULL,
   id_referent INT DEFAULT NULL,
-
-  FOREIGN KEY (id_referent)
-    REFERENCES referent(id_referent)
+  FOREIGN KEY (id_referent) REFERENCES referent(id_referent)
 );
 
-CREATE TABLE teilnehmer_themen (
+CREATE TABLE IF NOT EXISTS teilnehmer_themen (
   id_teilnehmer INT NOT NULL,
   id_themen INT NOT NULL,
 
